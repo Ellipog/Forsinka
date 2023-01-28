b138e247eb865215bca4975e10b3e59e41b780a2
Ellipog <elliotsaaen@hotmail.com>
2023-01-28 14:58:39 +0100

    Added better mobile support, and fixed bugs

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import Forsinkelse from "./components/Forsinkelse";
 import pako from "pako";
+import classNames from "classNames";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSchool, faBuilding } from "@fortawesome/free-solid-svg-icons";
 
 let count = 20;
 
@@ -9,7 +12,9 @@ function App() {
   const [forsinkelser, setForsinkelser] = useState([]);
   let [lineID, setLineID] = useState("");
   let [stopPlaceID, setStopPlaceID] = useState("60944");
-  const lines = [
+  const [activeFooterButton, setActiveFooterButton] = useState("60944");
+  const [prevStopPlaceID, setPrevStopPlaceID] = useState(stopPlaceID);
+  const ssLines = [
     {
       type: "Buss: ",
       line: "510",
@@ -47,6 +52,21 @@ function App() {
       line: "R20",
     },
   ];
+  const snLines = [
+    {
+      type: "Buss: ",
+      line: "515",
+    },
+    {
+      type: "Buss: ",
+      line: "520",
+    },
+  ];
+  const [lines, setLines] = useState(ssLines);
+  const linesMapping = {
+    60944: ssLines,
+    4977: snLines,
+  };
 
   async function fetchData() {
     fetch(`https://forsinkasrv.chillcraft.co/forsinkelser?limit=${count}&lineID=${lineID}&stopPlaceID=${stopPlaceID}`, {
@@ -67,41 +87,38 @@ function App() {
       });
   }
 
-  function handleLineIDChange(e) {
-    count = 20;
-    setLineID(e.target.value);
+  function fetchMoreData() {
+    count += 20;
+    fetchData();
   }
 
   useEffect(() => {
-    console.log(stopPlaceID + lineID);
+    setPrevStopPlaceID(stopPlaceID);
+  }, [stopPlaceID]);
+
+  useEffect(() => {
+    count = 20;
     fetchData();
-    count += 20;
   }, [lineID, stopPlaceID]);
 
   setTimeout(() => fetchData(), 60000);
 
   return (
     <div className="App">
-      <div className="stopList">
-        <button
-          onClick={() => {
-            count = 20;
-            setStopPlaceID(60944);
-          }}
-        >
-          Ski Stasjon
-        </button>
-        <button
-          onClick={() => {
-            count = 20;
-            setStopPlaceID(4977);
-          }}
-        >
-          Ski Næringspark
-        </button>
-      </div>
       <div className="header">
-        <select className="select" value={lineID} onChange={(e) => handleLineIDChange(e)}>
+        <select
+          className={classNames("select", "selectStopPlace")}
+          value={stopPlaceID}
+          onChange={(e) => {
+            setStopPlaceID(e.target.value);
+            setLines(linesMapping[e.target.value]);
+            setLineID("");
+          }}
+        >
+          <option value="60944">Ski Stasjon</option>
+          <option value="4977">Ski Næringspark</option>
+        </select>
+        <select className="select" value={lineID} onChange={(e) => setLineID(e.target.value)}>
           <option value="">Alle buss og tog</option>
           {lines.map((lines) => (
             <option key={lines.line} value={lines.line}>
@@ -113,9 +130,39 @@ function App() {
       {forsinkelser.map((forsinkelse, i) => {
         return <Forsinkelse key={i} line={forsinkelse.line} expectedTime={forsinkelse.expectedTime} aimedTime={forsinkelse.aimedTime} name={forsinkelse.name} />;
       })}{" "}
-      <button className="showMore" onClick={() => fetchData()}>
+      <button className="showMore" onClick={() => fetchMoreData()}>
         Vis mer...
       </button>
+      <div className="footer">
+        <button
+          className={`footerButton ${activeFooterButton === "60944" ? "activeFooterButton" : ""}`}
+          onClick={() => {
+            setLines(ssLines);
+            setStopPlaceID("60944");
+            setLineID("");
+            setActiveFooterButton("60944");
+          }}
+        >
+          <div className="footerIcon">
+            <FontAwesomeIcon icon={faBuilding} />
+          </div>
+          Ski Stasjon
+        </button>
+        <button
+          className={`footerButton ${activeFooterButton === "4977" ? "activeFooterButton" : ""}`}
+          onClick={() => {
+            setLines(snLines);
+            setStopPlaceID("4977");
+            setLineID("");
+            setActiveFooterButton("4977");
+          }}
+        >
+          <div className="footerIcon">
+            <FontAwesomeIcon icon={faSchool} />
+          </div>
+          Ski Næringspark
+        </button>
+      </div>
     </div>
   );
 }
